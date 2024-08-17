fd809ce729a8403e1984a23ffeb82810ac0b157a
[pankajdevo] <[pkcse20101991@gmail.com]>
2024-08-17 16:07:08 +0530

    this is testing

---
+++ ec2-create.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
